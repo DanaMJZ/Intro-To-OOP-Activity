c6c5afb9f04442a9be175dca38cd163ebb4d506f
Viktoriia <lyudkevych.v@gmail.com>
2025-03-31 12:22:24 -0700

    Created get_student_with_more_classes function

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,3 +1,6 @@
 # add your get_student_with_more_classes function here!
 def get_student_with_more_classes(claire, samara):
-    
+    if samara.get_num_classes() > claire.get_num_classes():
+       return samara
+    else:
+       return claire
--- activity/student.py
+++ activity/student.py
@@ -1,15 +1,15 @@
 # add your Student class here!
 class Student:
     def __init__(self, name, grade, classes):
+        
         self.name = name
-        self.grade = grade
+        self.grade_level = grade
         self.classes = classes
-    
+
     def add_class(self, class_name):
-        self.classes(class_name)
+       
+        return self.classes.append(class_name)
 
     def get_num_classes(self):
+        
         return len(self.classes)
-    
-    def summary(self):
-        return f"{self.name} is a {self.grade} enrolled in {self.classes}"
